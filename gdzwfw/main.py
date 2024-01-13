35c89b465e9e9f6b99191a1f3cd24d299318af08
yefeng <1226325909@qq.com>
2024-01-13 19:48:12 +0800

    第一个逆向

---
+++ gdzwfw/main.py
@@ -0,0 +1,103 @@
+# 这是一个示例 Python 脚本。
+
+# 按 Shift+F10 执行或将其替换为您的代码。
+# 按 双击 Shift 在所有地方搜索类、文件、工具窗口、操作和设置。
+
+import random
+import time
+import urllib
+from urllib.parse import unquote
+import hashlib
+import requests
+import json
+
+k = "k8tUyS$m"
+
+
+def getDate():
+    # 时间戳
+    t = int(round(time.time() * 1000))
+    return t
+
+
+
+
+def hne(n):
+    # 随机生成n个字符串
+
+    strs = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ'
+    return ''.join([random.choice(strs) for i in range(n)])
+
+
+def pne(e):
+    t = ""
+    if type(e) == dict:
+        t = '&'.join([f"{n}={e[n]}" for n in sorted(e.keys())])
+    elif type(e) == str:
+        t = '&'.join(sorted(e.split('&')))
+    return t
+
+
+def decodeURIComponent(url):
+    return unquote(url)
+
+
+def uK(param):
+    # sha256
+    return hashlib.sha256(param.encode('utf-8')).hexdigest()
+
+
+def t1(e={}):
+    t = e.get('p', '')
+    n = e.get('t', '')
+    u = e.get('n', '')
+    o = e.get('k', '')
+    r = pne(t)
+    strs = u + o + decodeURIComponent(r) + str(n)
+    return uK(strs)
+
+
+# s=pne('type=trading-type&openConvert=false&keyword=&siteCode=44&secondType=A&tradingProcess=&thirdType=%5B%5D&projectType=&publishStartTime=&publishEndTime=&pageNo=2&pageSize=10' )
+# print(s)
+
+
+# params = {"type": "trading-type", "openConvert": "false", "keyword": "", "siteCode": "44", "secondType": "A",
+#           "tradingProcess": "", "thirdType": "[]", "projectType": "", "publishStartTime": "", "publishEndTime": "",
+#           "pageNo": 3, "pageSize": 10}
+
+params = {
+    "type": "trading-type",
+    "openConvert": "false",
+    "keyword": "",
+    "siteCode": "44",
+    "secondType": "A",
+    "tradingProcess": "",
+    "thirdType": "[]",
+    "projectType": "",
+    "publishStartTime": "",
+    "publishEndTime": "",
+    "pageNo": 3,
+    "pageSize": 10
+}
+#将params转换为url
+# encoded_params = urllib.parse.urlencode(params)
+# print(encoded_params)
+# t=getDate()
+# n=hne(16)
+# p = 'type=trading-type&openConvert=false&keyword=&siteCode=44&secondType=A&tradingProcess=&thirdType=%5B%5D&projectType=&publishStartTime=&publishEndTime=&pageNo=2&pageSize=10'
+# e = {"p": encoded_params, "t": t, "n": n, "k": k}
+# print(e)
+# Signature = t1(e)
+
+
+def getSign(params):
+    t = getDate()
+    n = hne(16)
+
+    e={"p": urllib.parse.urlencode(params), "t": t, "n": n, "k": k}
+    #获取e中的p
+    return t1(e),e
+
+
+
+getSign(params)
---
+++ gdzwfw/toDataByParams.py
@@ -0,0 +1,62 @@
+import json
+import time
+
+import requests
+
+from main import hne, getSign
+
+param = {
+    "type": "trading-type",
+    "openConvert": "false",
+    "keyword": "",
+    "siteCode": "44",
+    "secondType": "A",
+    "tradingProcess": "",
+    "thirdType": "[]",
+    "projectType": "",
+    "publishStartTime": "",
+    "publishEndTime": "",
+    "pageNo": 3,
+    "pageSize": 10
+}
+def getData(param):
+    Signature,e=getSign(param)
+    headers = {
+        "Accept": "application/json, text/plain, */*",
+        "Accept-Language": "zh-CN,zh;q=0.9",
+        "Cache-Control": "no-cache",
+        "Connection": "keep-alive",
+        "Content-Type": "application/json",
+        "DNT": "1",
+        "Origin": "https://ygp.gdzwfw.gov.cn",
+        "Pragma": "no-cache",
+        "Referer": "https://ygp.gdzwfw.gov.cn/",
+        "Sec-Fetch-Dest": "empty",
+        "Sec-Fetch-Mode": "cors",
+        "Sec-Fetch-Site": "same-origin",
+        "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/120.0.0.0 Safari/537.36",
+        "X-Dgi-Req-App": "ggzy-portal",
+        "X-Dgi-Req-Nonce": e['n'],
+        "X-Dgi-Req-Signature": Signature,
+        "X-Dgi-Req-Timestamp": str(e['t']),
+        "sec-ch-ua": "\"Not_A Brand\";v=\"8\", \"Chromium\";v=\"120\", \"Google Chrome\";v=\"120\"",
+        "sec-ch-ua-mobile": "?0",
+        "sec-ch-ua-platform": "\"Windows\""
+    }
+    url = "https://ygp.gdzwfw.gov.cn/ggzy-portal/search/v2/items"
+    data = json.dumps(param, separators=(',', ':'))
+    response = requests.post(url, headers=headers, data=data)
+    return response.text
+
+
+if __name__ == '__main__':
+    param['pageNo']=4
+    page4=getData(param)
+    print("page4",page4)
+
+    #sleep 2s
+    time.sleep(2)
+
+    param['pageNo']=5
+    page5=getData(param)
+    print("page5",page5)
